f7c75553bfb0c362786cb10505b0771be99ef758
ajay-wohlig <ajay.yadav@wohlig.in>
2019-10-20 20:33:27 +0530

    initiAL commit

---
+++ src/app.js
@@ -0,0 +1,93 @@
+const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode')
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+//Define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public') 
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Weather App',
+        name: 'ajay'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about',{
+        title: 'Flower',
+        name: 'Ajay'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        msg: 'help msg',
+        title: 'Help',
+        name: 'Ajay'
+    })
+})
+
+app.get('/weather',(req,res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'Please provide address'
+        })
+    }
+    const address = req.query.address
+    geocode(address, (error, {latitude, longitude, location} = {}) => {
+        if(error){
+            return res.send({
+                error: error
+            })
+        }
+        forecast(latitude, longitude, (error, foreCastData) => {
+            if(error){
+                return res.send({
+                    error: error
+                })
+            }
+            res.send({
+                foreCastData,
+                location,
+                address
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Ajay',
+        errorMessage: 'Help article not found'
+
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Ajay',
+        errorMessage: 'Page not found'
+
+    })
+})
+
+app.listen(3001,() => {
+    console.log('Server has started has successfully')
+})
+
---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request")
+
+const geocode = (address,callback) => {
+    
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1IjoiYWpheTQyNTgiLCJhIjoiY2sweG1mZnNoMDZzODNucHJ1c3htYmtxbiJ9.iIt7YkuhuEtv4UFbnNwlLA&limit=1'
+    request({url, json: true},(error,{body})=>{
+        if(error){
+            callback("Cannot connect to location services",undefined)
+        }
+        else if(body.features.length == 0){
+            callback("Unable to find location try another search",undefined)
+        }
+        else{
+            callback(undefined,{
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+
+module.exports = geocode
